bd9deaf2c42b6a5c79a114801b96348fff8a0892
Miguel Pereyra <migvicpereyra@gmail.com>
2022-11-04 15:09:52 -0300

    prueba de  por email

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Config;
+use App\Mail\TestWhats;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +19,13 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/mailTest', function () {
+    $toSend = new TestWhats();
+    //$toSend->from = env('MAIL_FROM_ADDRESS');
+    //$toSend->assertSeeInHtml('migvicpereyra@hotmail.com');
+    //dd($toSend);
+    //print_r($_SERVER); die;
+    Mail::to('migvicpereyra@hotmail.com')->send($toSend);
+    echo "DONE";
 });
